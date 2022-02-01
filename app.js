e1d76e1cd6b38e6a8c4f42d5ae738c6ad30a4df0
Basilmp850 <68513395+Basilmp850@users.noreply.github.com>
2022-02-01 14:27:40 +0530

    deleted

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Hello");//This is the console statement..
 console.log("Hai");
-console.log("This is the modified commit");//commented
+//commented
 //changes made from remote repository...
